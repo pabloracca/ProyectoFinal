9f451313bf6c7abb4694b9970820c527e1f1ba61
pabloracca <123597435+pabloracca@users.noreply.github.com>
2023-01-25 18:55:53 -0300

    Add files via upload

---
+++ Java/JavaScript.js
@@ -0,0 +1,54 @@
+function Ocultar() {
+    var x = document.getElementById("Indice1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function OcultarF() {
+  var x = document.getElementById("Formación1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function OcultarE() {
+  var x = document.getElementById("Experiencia1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function OcultarId() {
+  var x = document.getElementById("Idiomas1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function OcultarC() {
+  var x = document.getElementById("Computación1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function OcultarO() {
+  var x = document.getElementById("Otros1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
